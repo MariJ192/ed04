0574b2d320dd051cebabfc202c32517dff121f56
MariJ192 <3155447@alu.murciaeduca.es>
2024-03-04 23:12:12 +0100

    Extraemos los metodos de agenda1

--- ed04/src/main/java/org/example/Main.java
+++ ed04/src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        initAgenda();
+    }
+
+    private static void initAgenda() {
         // Crear una nueva agendaContactos.
         Agenda agenda1 = new Agenda();
 
