0b5cda621bea69dc40c312c8d99d6a21617c38b3
MariJ192 <3155447@alu.murciaeduca.es>
2024-03-04 23:01:59 +0100

    Cambio el nombre de la variable por agenda1

--- ed04/src/main/java/org/example/Main.java
+++ ed04/src/main/java/org/example/Main.java
@@ -3,30 +3,30 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // Crear una nueva agendaContactos.
-        Agenda agendacontactos = new Agenda();
+        Agenda agenda1 = new Agenda();
 
         // Añadir contactos a la agendaContactos.
-        agendacontactos.addContact("John Doe", "1234567890");
-        agendacontactos.addContact("Jane Doe", "9876543210");
-        agendacontactos.addContact("Mary Jane", "1122334455");
+        agenda1.addContact("John Doe", "1234567890");
+        agenda1.addContact("Jane Doe", "9876543210");
+        agenda1.addContact("Mary Jane", "1122334455");
 
         // Imprimir todos los contactos de la agendaContactos.
         System.out.println("Todos los contactos: ");
-        for (Persona c : agendacontactos.getContacts()) {
+        for (Persona c : agenda1.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
 
         // Añadir un nuevo teléfono a un contacto existente.
-        agendacontactos.addContact("John Doe", "6543219876");
+        agenda1.addContact("John Doe", "6543219876");
 
         // Eliminar un contacto de la agendaContactos.
-        agendacontactos.removeContact("Jane Doe");
+        agenda1.removeContact("Jane Doe");
 
         // Modificar el teléfono de un contacto existente en la agendaContactos.
-        agendacontactos.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
+        agenda1.modifyPhoneNumber("John Doe", "1234567890", "6666666666");
 
         System.out.println("\nDespués de añadir, eliminar y modificar contactos: ");
-        for (Persona c : agendacontactos.getContacts()) {
+        for (Persona c : agenda1.getContacts()) {
             System.out.println(c.getName() + " -> " + c.getPhones());
         }
     }
